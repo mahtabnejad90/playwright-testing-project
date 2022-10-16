9f5438c83bdce4d3f9eadc6448f4577393524391
Mahtab Nejad <mahtab@bahmannejad.com>
2022-10-16 23:16:37 +0100

    annotations and describe block

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 
+
+test.describe('Basic test', () => {
 test("simple basic test", async({ page }) => {
 
     //navigates to website
@@ -10,74 +12,81 @@ test("simple basic test", async({ page }) => {
 
     //assertion to check value of selector
     await expect(pageTitle).toContainText("Example Domain")
-
+}) 
 })
 
-test("clicking on element", async({ page }) => {
+test.describe("input test suite", () => {
 
-    //navigates to website
-    await page.goto("http://zero.webappsecurity.com/index.html")
+    test("clicking on element", async({ page }) => {
 
-    //click on element id. # is used to access id
-    await page.click("#signin_button")
-
-    await page.click("text=Sign in")
-
-    const loginErrorMessage = await page.locator(".alert-error")
-    await expect(loginErrorMessage).toContainText("Login and/or password are wrong.")
+        //navigates to website
+        await page.goto("http://zero.webappsecurity.com/index.html")
     
-})
-
-/* test("Selectors", async({ page }) => {
-
-    await page.click("text=some text")
-
-    //css selectors
-    await page.click("button")
-    await page.click("#id")
-    await page.click(".class")
-
-    //visible css selectors
-    await page.click(".submit-button:visible")
+        //click on element id. # is used to access id
+        await page.click("#signin_button")
     
-    // combos
-    await page.click("#username .first")
-
-    //xpath selectors
-    await page.click("//button")
-
-
-}) */
-
-test("Working with inputs", async({ page }) => {
-
-    //navigates to website
-    await page.goto("http://zero.webappsecurity.com/index.html")
-
-    await page.click("#signin_button")
-
-    await page.fill("#user_login", "username")
-    await page.type('#user_login', 'some username')
-    await page.type('#user_password', 'some password')
-    await page.click("text=Sign in")
-    const loginErrorMessage = await page.locator(".alert-error")
-    await expect(loginErrorMessage).toContainText("Login and/or password are wrong.")
+        await page.click("text=Sign in")
+    
+        const loginErrorMessage = await page.locator(".alert-error")
+        await expect(loginErrorMessage).toContainText("Login and/or password are wrong.")
+        
+    })
+    
+    test("Working with inputs", async({ page }) => {
+    
+        //navigates to website
+        await page.goto("http://zero.webappsecurity.com/index.html")
+    
+        await page.click("#signin_button")
+    
+        await page.fill("#user_login", "username")
+        await page.type('#user_login', 'some username')
+        await page.type('#user_password', 'some password')
+        await page.click("text=Sign in")
+        const loginErrorMessage = await page.locator(".alert-error")
+        await expect(loginErrorMessage).toContainText("Login and/or password are wrong.")
+    
+    })
 
 })
 
-test("Assertions", async({ page }) => {
+test.describe("Selectors and assertions", () => {
+    test.skip("Selectors", async({ page }) => {
 
-    //navigates to website
-    await page.goto("https://example.com")
-    await expect(page).toHaveURL("https://example.com")
-    await expect(page).toHaveTitle("Example Domain")
-    const elementHOne = await page.locator("h1")
-    await expect(elementHOne).toBeVisible()
-    await expect(elementHOne).toHaveText("Example Domain")
-    await expect(elementHOne).toHaveCount(1)
-
-    const elementNonExisting = await page.locator("h5")
-    await expect(elementNonExisting).not.toBeVisible()
-
-
+        await page.click("text=some text")
+    
+        //css selectors
+        await page.click("button")
+        await page.click("#id")
+        await page.click(".class")
+    
+        //visible css selectors
+        await page.click(".submit-button:visible")
+        
+        // combos
+        await page.click("#username .first")
+    
+        //xpath selectors
+        await page.click("//button")
+    
+    
+    })
+    
+    //.only executes only annotated test
+    test("Assertions", async({ page }) => {
+    
+        //navigates to website
+        await page.goto("https://example.com")
+        await expect(page).toHaveURL("https://example.com")
+        await expect(page).toHaveTitle("Example Domain")
+        const elementHOne = await page.locator("h1")
+        await expect(elementHOne).toBeVisible()
+        await expect(elementHOne).toHaveText("Example Domain")
+        await expect(elementHOne).toHaveCount(1)
+    
+        const elementNonExisting = await page.locator("h5")
+        await expect(elementNonExisting).not.toBeVisible()
+    
+    
+    })
 })

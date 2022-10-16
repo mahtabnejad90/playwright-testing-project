0cd484dfdad845aa7b52e5baec5e62e8e42be58b
Mahtab Nejad <mahtab@bahmannejad.com>
2022-10-16 18:30:03 +0100

    clickable elements

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -12,3 +12,22 @@ test("simple basic test", async({ page }) => {
     await expect(pageTitle).toContainText("Example Domain")
 
 })
+
+test("clicking on element", async({ page }) => {
+
+    //navigates to website
+    await page.goto("http://zero.webappsecurity.com/index.html")
+
+    //click on element id. # is used to access id
+    await page.click("#signin_button")
+
+    await page.click("text=Sign in")
+
+    const loginErrorMessage = await page.locator(".alert-error")
+    await expect(loginErrorMessage).toContainText("Login and/or password are wrong.")
+    
+
+
+
+
+})

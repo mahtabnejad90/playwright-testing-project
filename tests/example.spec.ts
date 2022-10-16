8713244fed5ee143b6956acd4a1be3c341971852
Mahtab Nejad <mahtab@bahmannejad.com>
2022-10-16 23:01:43 +0100

    user inout

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -26,8 +26,41 @@ test("clicking on element", async({ page }) => {
     const loginErrorMessage = await page.locator(".alert-error")
     await expect(loginErrorMessage).toContainText("Login and/or password are wrong.")
     
+})
+
+/* test("Selectors", async({ page }) => {
+
+    await page.click("text=some text")
+
+    //css selectors
+    await page.click("button")
+    await page.click("#id")
+    await page.click(".class")
+
+    //visible css selectors
+    await page.click(".submit-button:visible")
+    
+    // combos
+    await page.click("#username .first")
+
+    //xpath selectors
+    await page.click("//button")
 
 
+}) */
 
+test("Working with inputs", async({ page }) => {
+
+    //navigates to website
+    await page.goto("http://zero.webappsecurity.com/index.html")
+
+    await page.click("#signin_button")
+
+    await page.fill("#user_login", "username")
+    await page.type('#user_login', 'some username')
+    await page.type('#user_password', 'some password')
+    await page.click("text=Sign in")
+    const loginErrorMessage = await page.locator(".alert-error")
+    await expect(loginErrorMessage).toContainText("Login and/or password are wrong.")
 
 })

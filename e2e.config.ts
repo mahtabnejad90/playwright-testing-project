16fcd4999bfc716b353500ad202f8678e690cb5d
Mahtab Bahman Nejad <mahtab@firstbasehq.com>
2022-10-23 20:49:29 +0100

    add e2e config

---
+++ e2e.config.ts
@@ -0,0 +1,41 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+import dotenv from 'dotenv';
+import path from 'path';
+
+//require('dotenv').config();
+
+//dotenv.config();
+require('dotenv').config({ path: 'local.env' })
+
+
+
+const config: PlaywrightTestConfig = {
+    timeout: 60000,
+    testDir: 'tests/e2e',
+    retries: 0,
+    use: {
+        //baseURL: process.env.BASE_URL,
+        headless: true,
+        viewport: { width: 1280, height: 720 },
+        actionTimeout: 10000,
+        ignoreHTTPSErrors: true,
+        video: "off",
+        screenshot: "off",
+    },
+    projects: [
+        {
+            name: "Chromium",
+            use: {browserName: "chromium"},
+        },
+        {
+            name: "Firefox",
+            use: {browserName: "firefox"},
+        },
+        {
+            name: "Webkit",
+            use: {browserName: "webkit"},
+        }
+    ]
+}
+
+export default config;
